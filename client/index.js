41c0822d036b46ee70a5e0e365bc9c82aed29406
Levi Milli <levi.millikin@gmail.com>
2023-06-18 10:28:09 -0600

    toys working

--- client/index.js
+++ client/index.js
@@ -4,12 +4,26 @@ const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
+let merklin = new MerkleTree(niceList)
+
+const root = merklin.getRoot();
+
+const name = 'Anna Stehr';
+const index = niceList.findIndex(n => n === name);
+const proof = merklin.getProof(index);
+console.log(index, root, proof)
+
+
+
+
 async function main() {
   // TODO: how do we prove to the server we're on the nice list? 
-
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name: name,
+    proof: proof,
+    root: root
   });
+  
 
   console.log({ gift });
 }
